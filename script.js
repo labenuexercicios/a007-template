775503cd07b272caa9179ad4385941b799c0c9d3
Joanadayse <daysejoana3@gmail.com>
2022-07-18 22:01:15 -0300

    Exercicios-dia7

---
+++ script.js
@@ -0,0 +1,41 @@
+//let nome = prompt ("qual a seu nome?") // convertendo tipos usando o prompt 
+//let idade = prompt ("qual a sua idade?")
+
+//console.log ('ola meu nome é', nome , 'tenho' , idade , "anos")
+ 
+//let tenhoPet = true
+//const nomeDoPet = "Lili"
+
+//console.log (typeof nome ) // diz que tipo é 
+//console.log (typeof idade.toString )
+//console.log (typeof tenhoPet )
+//console.log (typeof nomeDoPet )
+
+//nome = Number ("Joana")
+
+// exercicio de fixação 
+//const filho = "Matheus"
+//let idade = 13
+//let queroMais = false
+
+//console.log (typeof filho)
+//console.log (typeof idade)
+//console.log (typeof queroMais)
+
+// exercicio de fixação
+// Crie duas variaveis , A e B e atribua um valor numerico para cada uma delas;
+// crie uma terceira variavel C e não atribua valor
+// agora reatribua valores de forma a trocar de lugar
+//assim o valor de A seria o valor de B e virse e versa 
+
+
+let a = 30;
+let b = 20;
+let c;
+c=a
+a=b
+b=c
+ 
+console.log ( "A",a)
+console.log ( "B" ,b)
+console.log ("C" , c)
