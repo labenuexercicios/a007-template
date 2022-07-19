ac17db4e387a53160d0df26dacfe669e0b112806
Eduardo Boucault <boucault.labenu@outlook.com>
2022-07-18 21:52:37 -0300

    Aula 18/07/2022, variáveis e tipos primitivos

---
+++ script.js
@@ -0,0 +1,62 @@
+// Este é um comentário de linha.
+
+/* Este
+é um
+comentário
+de bloco, estes
+foram os testes 
+de comentários */
+
+// PRATICAR REVISÃO DE CÓDIGOS
+
+const meuNome = "Eduardo Lima Boucault"
+console.log(meuNome)
+let idade = 29
+console.log(idade)
+
+const nomeMae = "Gilda Lima da Silva"
+console.log(nomeMae)
+let idadeMae = 46
+console.log(idadeMae)
+
+const nomeCachorro = "Pituka"
+console.log(nomeCachorro)
+let idadeDog = 11
+console.log(idadeDog)
+
+console.log(meuNome , nomeMae , nomeCachorro)
+console.log(idade , idadeMae , idadeDog)
+
+console.log("Meu é" , meuNome , "minha mãe se chama" , nomeMae , "e aqui em casa nos temos uma cachorrinha chamada" , nomeCachorro , "ela tem," , idadeDog , "anos.")
+
+let pet = true
+
+let nome2 = Number (prompt("Qual seu nome?"))
+let idade2 = Number(prompt("Qual é sua idade?"))
+console.log('Meu nome é' , nome2 , 'e eu tenho', idade2 , 'anos.')
+
+console.log(typeof nome2)
+console.log(typeof idade2)
+
+//*Exercícío de fixação 1
+
+const objeto = "caneta"
+let numeroDeCanetas = 2
+let corAzul = true
+
+console.log(typeof objeto)
+console.log(typeof numeroDeCanetas)
+console.log(typeof corAzul)
+
+//*Exercício de fixação 2
+
+let v1 = 30;
+let v2 = 20;
+let v3 ;
+
+v3 = v1
+v1 = v2
+v2 = v3
+
+console.log("A" , v1)
+console.log("B" , v2)
