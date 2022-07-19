1cb546d8146ae005b9e74ca5095ed62b25c5e436
JulianDiel <drdiel@hotmail.com>
2022-07-18 21:44:16 -0300

    Exercicios de aula, reenviando

---
+++ script.js
@@ -0,0 +1,62 @@
+// const nome = "J. Julian D.";
+// let idade = 30;
+
+// console.log(`Olá! Meu nome é `,nome, `e eu tenho `, idade, `anos);
+
+// nome = "Jennifer";
+// idade = 26;
+// let redeSocial = "Tinder";
+// console.log(nome);
+// console.log(idade);
+
+// console.log(`O nome dela é `, nome, `eu conheci ela no `, redeSocial, `ela tem `, idade, `anos`);
+
+// let tenhoPet = false;
+// const nomeDoPet = undefined;
+
+// console.log(typeof tenhoPet, typeof nomeDoPet );
+
+// let nome = prompt("Qual é o seu nome? ");
+// let idade = Number(prompt("qual é sua idade"));
+
+// console.log(`Meu nome é `, nome, `e tenho `, idade, `anos`);
+// console.log(typeof nome, typeof idade); //string number
+
+// let nome = Number(prompt("Qual é o seu nome? "));
+// let idade = (prompt("qual é sua idade"));
+
+// console.log(`Meu nome é `, nome, `e tenho `, idade, `anos`);
+// console.log(typeof nome, typeof idade); //number string
+
+
+//EXERCICIO FIXAÇÃO
+
+// const filme = prompt("Qual seu filme ou sério preferida? ");
+// let notaDoFilme = Number(prompt("De uma nota de 0 a 10 para ele(a)"))
+// let recomenda = confirm("Você recomenda ele(a)?");
+    
+// console.log(`Meu filme ou série preferida é `, filme, `e sua nota é`, notaDoFilme);
+// console.log(recomenda);
+// console.log(typeof filme, typeof notaDoFilme, typeof recomenda); //string number boolean
+
+// const filme = "Suits";
+// let notaDoFilme = 10
+// let recomenda = confirm("Você recomenda ele(a)?");
+    
+// console.log(`Meu filme ou série preferida é `, filme, `e sua nota é`, notaDoFilme,`.`);
+// console.log(recomenda);
+// console.log(typeof filme, typeof notaDoFilme, typeof recomenda); //string number boolean
+
+// let a = 30;
+// let b = 20;
+// let c = "";
+
+// console.log("A" +a);
+// console.log("B"+b);
+
+// c=a;
+// a=b;
+// b=c;
+
+// console.log("a "+ a);
+// console.log("b "+b); //A30, B20, a20,b30
