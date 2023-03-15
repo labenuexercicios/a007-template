fb640c5c630b7b121335f34d2305f157586cdfc3
caroole01~ <caroline_voleck@hotmail.com>
2023-03-15 14:54:06 -0300

    pronto

--- script.js
+++ script.js
@@ -1,1 +1,88 @@
+//Exercício 1 Parte 1
 
+//a)
+
+//let nome = (prompt("Qual seu nome?"));;
+
+//b)
+
+//let idade = (prompt("Qual sua idade?"));
+
+//c)
+
+//console.log(typeof nome);
+//console.log(typeof idade);
+
+//d) Reflita: por que esse tipo foi impresso? Escreva a resposta em um comentário de código.
+// Para visualizarmos tipos de retornos impressos.
+
+//e) Atualização
+
+//f) Continuam do type String
+
+//g) 
+
+//console.log("Meu nome é," +" " + nome + " " + "e tenho" + " " + idade + " " + "anos de idade.");
+
+//Parte 2
+
+//a)
+
+//let pergunta1 = (prompt("Qual sua cor favorita?"));
+//let pergunta2 = (prompt("Qualsua banda favorita?"));
+//let pergunta3 = (prompt("Qual sua comida favorita?"));
+
+//b)
+///console.log(pergunta1);
+//console.log(pergunta2);
+//console.log(pergunta3);
+
+
+//d)
+
+//const perguntaA = (prompt("Você está usando uma roupa azul hoje?"));
+//const respostaA = (prompt(perguntaA));
+
+//console.log(perguntaA, respostaA);
+
+
+//Exercício 2
+
+
+let a = 10;
+let b = 25;
+let c = a;
+a = b;
+b = c;
+
+//console.log(a); 
+//console.log(b); 
+
+//Exercício 3
+
+let programa1 = (prompt("Nome completo"));
+let programa2 = (prompt(" Data de nascimento (utilize as / )"));
+let programa3 = prompt("Endereço");
+let programa4 = (prompt("CPF"));
+let programa5 = (prompt("Escolaridade"));
+let programa6 = (prompt("Possui CHN?"));
+let programa7 = (prompt("Quantos filhes possui?"));
+let programa8 = (prompt("Cargo atual"));
+let programa9 = (prompt("Salário"));
+let programa10 = (prompt("Recebe comissão? Se sim, informe a porcentagem, caso não, responda com zero"));
+let programa11 = (prompt("Informe o ano de admissão"));
+
+let conversao = toString(programa2,programa4,programa7,programa9,programa10,programa11);
+
+console.log(typeof programa1);
+console.log(typeof programa2);
+console.log(typeof programa3);
+console.log(typeof programa4);
+console.log(typeof programa5);
+console.log(typeof programa6);
+console.log(typeof programa7);
+console.log(typeof programa8);
+console.log(typeof programa9);
+console.log(typeof programa10);
+console.log(typeof programa11);
+console.log("Este é meu relatório de pessoa colaboradora");
