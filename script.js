8460302ac81f531d1a9279fd6fe6d2b71fdc477a
Giovanni Souza <GSMartins999>
2023-05-02 21:22:25 -0300

    variaveis

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const nome = "Giovanni"
+
+let cidade = prompt("Digite sua cidade")
+
+let bairro = prompt("Digite seu bairro")
+
+let numero =  Number(prompt("Digite seu número"))
+
+let confirmar = confirm("Você deseja confirmar seu endereço?")
+
+console.log(nome)
+console.log("Moro em", cidade, "no", bairro, " no número:", numero, confirmar)
 
