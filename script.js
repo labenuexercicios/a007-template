b516a7b18edb0f809d1412d7e1d8fc56e5c8dcb4
Rafael Machado <machadorafael27@gmail.com>
2023-05-02 21:33:09 -0300

    Aulas variaveis

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 
+/*console.log("Hello world!");
+
+let nome = prompt("Digite seu nome:");
+let idade = Number(prompt("Digite a sua idade:"));
+let presenca = confirm("Confirme sua presença:");
+
+console.log(nome);
+console.log(idade);
+console.log(presenca);
+
+console.log(` Estudante, ${nome},tem ,${idade}, anos. Presença:${presenca}`);*/
+
+console.log("Hj vamos alugar um apartamento para 3 pessoas.")
+
+let apartamento = prompt("Qual é o nome do apartamento:")
+let pessoa1 = prompt("Nome da pessoa:")
+let Idade1 = prompt("Idade da pessoa:")
+let pessoa2 = prompt("Nome da pessoa:")
+let Idade2 = prompt("Idade da pessoa:")
+let pessoa3 = prompt("Nome da pessoa:")
+let Idade3 = prompt("Idade da pessoa:")
+console.log("------------------------------------------------------")
+console.log(` O nome da primeira pessoa é, ${pessoa1},tem ,${Idade1}, anos.`);
+console.log(` O nome da segunda pessoa é, ${pessoa2},tem ,${Idade2}, anos.`);
+console.log(` O nome da terceira pessoa é, ${pessoa3},tem ,${Idade3}, anos.`);
+
+
+
+
+
+
+/*let pessoasIdade= []
+for(i<0;i<=3;i++){
+    let pessoa = prompt("Nome da pessoa:")
+    let Idade = prompt("Idade da pessoa:")
+    pessoasIdade.push(pessoa,idade)
+}
+
+for(const pes of pessoasIdade){
+    console.log(` O nome da primeira pessoa é, ${pes.pessoa},tem ,${pes.Idade}, anos.`);
+}*/
+
+
+
+
+
