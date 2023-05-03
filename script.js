022b3fdf18a6f565b0402b53afe007e42bf4385d
ezequiel <ezequielvieira.77@gmail.com>
2023-05-03 03:49:18 -0300

    se joga player e vamos jogar então

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let nome = prompt("digite seu nome")
+let nome = prompt("me diga seu nome jovem lenda")
 
-let idade = Number(prompt("digite sua idae"))
+let idade = Number(prompt("digite aqui sua existencia"))
 
-let presenca = confirm("digite sua prenca")
+let presenca = confirm("ja chegou?")
 
 console.log(typeof nome)
 console.log(typeof idade)
 console.log(typeof presenca)
 
-console.log("estudante", nome, "tem", idade, "anos. presenca", presenca)
+console.log("ola viajante seja bem vindo, me diga suas credenciais para irmos juntos nesse fantastico segredo", nome, "me deiga agora o seu maior segredo, sua idade.", idade, "você vem?", presenca) 
