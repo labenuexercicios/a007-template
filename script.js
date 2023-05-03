f788f37dad623b0c29160c384afa029e73cdf78b
ezequiel <ezequielvieira.77@gmail.com>
2023-05-02 21:15:31 -0300

    exercicios

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let nome = prompt("digite seu nome")
 
+let idade = Number(prompt("digite sua idae"))
+
+let presenca = confirm("digite sua prenca")
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof presenca)
+
+console.log("estudante", nome, "tem", idade, "anos. presenca", presenca)
