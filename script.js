6e08a1b0f67211a9a4f16aff9d78049cc0e0d068
Douglasluis508 <douglasluis508@gmail.com>
2023-05-05 18:30:13 -0300

    exercicios variaveis

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+  // let nome =(prompt("Seu nome"));
+  // console.log(typeof nome);
+  // let idade =Number(prompt("Sua idade"));
+  // console.log(typeof idade);
+  // let presenca =confirm (true);
+  // console.log(typeof presenca);
 
+  // console.log("Estudante", nome, "tem", idade, "anos. Presenca:", true)
+
+  // console.log(typeof nome, typeof idade, typeof presenca)
+
+  let nome =(prompt("Qual seu nome?"));
+  console.log(nome) 
+  const celular =(prompt("Qual modelo do seu celular"));
+  console.log(typeof celular);
+  
+  let tempo =Number(prompt("Há quanto tempo você não troca seu celular?"));
+  console.log(typeof valor);
+
+  let troca = confirm(prompt("Você prentende trocar de celular?"));
+  console.log(typeof troca);
+
+  console.log("Ola", nome, "você possui um", celular, "Há quanto tempo? , voce pretende trocar de celular?", troca)
+
+ 
