91b83f27dfa6dec10a3f2b36551e21964e286294
Gilson Sebastião <gilsoncbsjr@gmail.com>
2023-07-10 21:45:26 -0300

    Exercicio 1, 2, e 3 feitos

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const nome = prompt("Qual o seu nome?")
+let idade = Number(prompt("Qual sua idade?"))
+let presença = confirm("Veio à aula hoje?")
+
+console.log(nome, idade, presença)
+
+console.log("nome:",typeof nome,"idade:",typeof idade,"presença:",typeof presença)
+
+console.log("Estudante", nome, "tem", idade, "anos. Presença:", presença);
 
